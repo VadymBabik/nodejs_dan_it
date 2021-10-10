69680513049514e76ccac8ff98aee3a4cae5b4cd
Anton N <anton.nikolenko@medentee.com>
2021-10-10 13:09:54 +0300

    end lesson 1

---
+++ lesson 1/typescript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tools_1 = require("./tools");
+(0, tools_1.test)();
+let name = 'ssss';
+let num;
+const user = {
+    name: 'asdsa',
+};
+//# sourceMappingURL=index.js.map
---
+++ lesson 1/typescript/src/index.ts
@@ -0,0 +1,26 @@
+import { test } from './tools';
+
+test();
+
+// ! number;
+// ! string;
+// ! boolean;
+// ! Symbol;
+// ! object;
+
+let name = 'ssss';
+let num: number;
+
+interface IId {
+  id: number;
+}
+interface IUser {
+  name: string;
+
+  email: string;
+}
+
+const generic = {
+  name: 'asdsa',
+  id: 121,
+};
